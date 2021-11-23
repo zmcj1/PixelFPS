e8992df81ab29399bdf8a4a895c26a28d74fb31d
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-23 09:33:50 +0300

    Update source.cpp

--- src/PixelFPSDemo/source.cpp
+++ src/PixelFPSDemo/source.cpp
@@ -1,4 +1,11 @@
-﻿#define OLC_PGE_APPLICATION
+﻿//  PixelFPS
+//  author : zmcj1
+//  date : 2021 / 11 / 23
+//  game repo : https://github.com/zmcj1/PixelFPS
+//  game engine : https://github.com/OneLoneCoder/olcPixelGameEngine
+//  massive massive thx javidx9!
+
+#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include <cmath>
 #include <list>
