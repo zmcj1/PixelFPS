c2454020ce9f5a1160e8ceeecb4a3ff83e886724
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-24 06:41:43 +0300

    add raycastWorldExample.cpp

---
+++ src/PixelFPSDemo/RaycastWorldExample.cpp
@@ -0,0 +1,17 @@
+﻿//  PixelFPS
+//  author : zmcj1
+//  date : 2021 / 11 / 24
+//  game repo : https://github.com/zmcj1/PixelFPS
+//  game engine : https://github.com/OneLoneCoder/olcPixelGameEngine
+//  massive massive thx javidx9!
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+using namespace olc;
+using namespace std;
+
+int main()
+{
+    return 0;
+}
--- src/PixelFPSDemo/source.cpp
+++ src/PixelFPSDemo/source.cpp
@@ -1,6 +1,6 @@
 ﻿//  PixelFPS
 //  author : zmcj1
-//  date : 2021 / 11 / 23
+//  date : 2021 / 11 / 24
 //  game repo : https://github.com/zmcj1/PixelFPS
 //  game engine : https://github.com/OneLoneCoder/olcPixelGameEngine
 //  massive massive thx javidx9!
