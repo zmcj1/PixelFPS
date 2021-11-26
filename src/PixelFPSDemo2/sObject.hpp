1b4632dd6d0a8cc3556456d4311338018129814f
GreatDestroyerCharlie <1669247240@qq.com>
2021-11-26 15:11:07 +0300

    update PixelFPSDemo2

---
+++ src/PixelFPSDemo2/sObject.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "olcSprite.hpp"
+
+class sObject
+{
+public:
+    float x;
+    float y;
+    //velocity:
+    float vx = 0;
+    float vy = 0;
+    bool remove = false;
+    olcSprite* sprite;
+
+    //collision with wall and border of map
+    bool enableCollision = true;
+    bool enableLifeTime = false;
+    float lifeTime = 1.0f; //just a defualt value
+
+    sObject(float x, float y, olcSprite* sprite)
+    {
+        this->x = x;
+        this->y = y;
+        this->sprite = sprite;
+    }
+};
---
+++ src/PixelFPSDemo2/tool.hpp
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace olc;
+
+void debug_output_line(const wstring& info)
+{
+    OutputDebugString((info + L"\n").c_str());
+}
+
+//writeline:
+void debug_output_vector2(const vi2d& vec2)
+{
+    debug_output_line(to_wstring(vec2.x) + L" " + to_wstring(vec2.y));
+}
+
+//from:https://stackoverflow.com/questions/612097/how-can-i-get-the-list-of-files-in-a-directory-using-c-or-c
+//extension without dot(.)
+vector<wstring> get_all_files_names_within_folder(const wstring& folder, const wstring& extension)
+{
+    vector<wstring> names;
+    wstring search_path = folder + L"/*." + extension;
+    WIN32_FIND_DATA fd;
+    HANDLE hFind = ::FindFirstFile(search_path.c_str(), &fd);
+    if (hFind != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            // read all (real) files in current folder
+            // , delete '!' read other 2 default folder . and ..
+            if (!(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+            {
+                names.push_back(fd.cFileName);
+            }
+        }
+        while (::FindNextFile(hFind, &fd));
+        ::FindClose(hFind);
+    }
+    return names;
+}
